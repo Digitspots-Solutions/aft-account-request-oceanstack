806b5d76399d686b9aae366b77d794bb15d4b5fc
Ravencodess <115118888+Ravencodess@users.noreply.github.com>
2026-02-27 23:04:06 +0100

    Add account request for thedatastreamtech-admin (admin@thedatastreamtech.com)

--- terraform/main.tf
+++ terraform/main.tf
@@ -444,3 +444,33 @@ module "thecyberspheretechnologies-com-admin" {
 
   account_customizations_name = "sandbox"
 }
+
+module "thedatastreamtech-com-admin" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "admin@thedatastreamtech.com"
+    AccountName               = "thedatastreamtech-admin"
+    ManagedOrganizationalUnit = "Websites Workload (ou-7i6x-pme73pq0)"
+    SSOUserEmail              = "admin@thedatastreamtech.com"
+    SSOUserFirstName          = "thedatastreamtech"
+    SSOUserLastName           = "thedatastreamtech"
+  }
+
+  account_tags = {
+    "Opportunity" = "true"
+    "ManagedBy" = "OpportunityPortal"
+    "ProvisioningRetry" = "2026-02-27T22:04:06.180927"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Opportunity Portal"
+    change_reason       = "Self-service account creation"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
